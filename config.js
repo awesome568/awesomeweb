2a7b29d70fb733bdbcd623000d07670790e40fbd
awesome231 <114026907+awesome568@users.noreply.github.com>
2024-03-06 17:14:31 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 window.__CONFIG__ = {
   // The URL for the CORS proxy, the URL must NOT end with a slash!
   // If not specified, the onboarding will not allow a "default setup". The user will have to use the extension or set up a proxy themselves
-  VITE_CORS_PROXY_URL: "",
+  VITE_CORS_PROXY_URL: "https://dev--resonant-cendol-a42cc3.netlify.app",
 
   // The READ API key to access TMDB
-  VITE_TMDB_READ_API_KEY: "CHANGEME",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkOWVlZjlmNmU1ZDYwMWM1OTJmZjIyMGRiOGM2NDRjMyIsInN1YiI6IjY1ZDdkZjY1MWIxNTdkMDE0YmJiNTRiMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.RRJBuYwR8LwQFjTgSsWqi3jDSnDUBqF13rxqTobsPu8",
 
   // The DMCA email displayed in the footer, null to hide the DMCA link
   VITE_DMCA_EMAIL: null,
